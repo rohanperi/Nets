763e3e41a18cf759358d136da2ccd0708269d76f
rohanperi <perirohan@gmail.com>
2016-11-05 16:51:56 -0400

    scroll tab code input

--- app/src/main/java/com/example/rohan/nets/MainActivity.java
+++ app/src/main/java/com/example/rohan/nets/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.rohan.nets;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 
 public class MainActivity extends FragmentActivity {
 
+    ViewPager viewPager=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        viewPager.setAdapter(new MyAdapter(fragmentManager));
+    }
+}
+
+class MyAdapter extends FragmentPagerAdapter {
+
+    public MyAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        Fragment fragment=null;
+        if (i==0) {
+            fragment=new Menu();
+        }
+        if (i==1) {
+            fragment=new News();
+        }
+        if (i==2) {
+            fragment=new Options();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
     }
 }
